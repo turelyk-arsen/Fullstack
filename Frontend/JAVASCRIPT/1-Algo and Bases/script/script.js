fc2bddbf2d10b1d4f70f05d5cc371b0f1ec44e99
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-02-23 15:30:43 +0100

    23/02/2023

---
+++ Frontend/JAVASCRIPT/1-Algo and Bases/script/script.js
@@ -0,0 +1,58 @@
+/*
+   Multiple line 
+   of comment
+*/
+
+// One line comment
+// help you to disable a line of code
+
+// TO show off
+
+/*************************
+ ****Decoration***********
+ ************************/
+
+ // * Important information is highlighted
+ // ! Deprecated method, DO NOT USE
+ // ? Should this method exposed in the public 
+ // TODO : change this function so that it do this
+
+
+// Already defined FUNCTION
+
+// [2 input] - [3 OUTPUTS]
+
+// INPUT : prompt("sentence");
+// const name = prompt('Enter your name');
+
+// INPUT : confirm('question');
+// const legal = confirm('Are you above 18?');
+
+
+// OUTPUT : alert('message');
+// alert('Hello!');
+
+// OUTPUT : document.write('<h2>ok</h2>);
+document.write('<h2>ok</h2>');
+
+// OUTPUT for the devs : console.log(''); 
+console.log("I'm a dev now");
+
+
+
+// VARIABLES 
+const teacher = 'Gabriel'; // Declaration + Assignment
+// teacher = 'Tiago'; ERROR 
+let age;  // Declaration
+age = 39; // assignment / initialisation
+age = 40;
+let lastName = 'Abreu'; // use camelCase
+console.log('teacher');
+console.log(teacher);
+console.log(age);
+
+// don't use VAR
+// Var is for loser
+// * you have to use "const" and "let"
+
+
