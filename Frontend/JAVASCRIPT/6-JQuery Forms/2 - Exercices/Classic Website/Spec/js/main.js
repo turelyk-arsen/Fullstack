7df9c6ae8e2fa049f8ef95bfcc9f7ab490096688
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-03-02 16:41:41 +0100

    02/03/2023 evening

--- Frontend/JAVASCRIPT/6-JQuery Forms/2 - Exercices/Classic Website/Spec/js/main.js
+++ Frontend/JAVASCRIPT/6-JQuery Forms/2 - Exercices/Classic Website/Spec/js/main.js
@@ -3,6 +3,49 @@
 // 2 - When the user submit his Form, you need to do some verifications
 // You will use #helpForm to warn the user
 // Trigger
+$("form").on("submit", function (event) {
+  event.preventDefault();
+  let errors = [];
+  // inputs
+  const nameValue = $("#name").val();
+  const subjectValue = $("#subject").val();
+  const messageValue = $("#message").val();
+  const emailValue = $("#email").val();
+
+  // logic
+  const nameValid = nameValue.length > 0;
+  if (!nameValid) {
+    errors.push("Name is required");
+  }
+
+  const subjectValid = subjectValue.length > 0;
+  if (!subjectValid) {
+    errors.push("Subject is required");
+  }
+
+  const messageValid = messageValue.length > 40;
+  if (!messageValid) {
+    errors.push("Message should be at least 40");
+  }
+
+  const emailValid = emailValue.includes("@") && emailValue.includes(".");
+  if (!emailValid) {
+    errors.push("That is not a valid email");
+  }
+  $("#helpForm").html(errors.join("<br>"));
+
+  if (errors.length == 0) {
+    const success =
+      nameValue +
+      ", your message" +
+      messageValue +
+      " has been sent. You will be contact soon on the mail address " +
+      emailValue +
+      " about" +
+      subjectValue;
+    $("form").text(success);
+  }
+});
 // Output
 //errors
 // .join('<br>') will make a string from my array
@@ -16,18 +59,21 @@
 
 $("#preloader").fadeOut(1000);
 
-$("form").on("submit", function (event) {
-  event.preventDefault();
+// $("form").on("submit", function (event) {
+//   event.preventDefault();
 
-  const message = $("#message").val();
-  const messageValid = message.length > 4;
+//   const message = $("#message").val();
+//   const messageValid = message.length > 4;
 
 //   const email = $("#email").val();
-//   const emailValid = email.match;
+//   const emailatSign = email.match(/@/);
+//   const emailDot = email.match(/./);
+//   const emailValid = emailDot && emailatSign;
 
-  if (messageValid) {
-    $("form").html("<h2> You will be contact soon on the mail address </h2>");
-  } else {
-    $('#mymes').text("The message must be more than 40 characters:");
-  }
-});
+//   if (messageValid && emailValid) {
+//     $("form").html("<h2> You will be contact soon on the mail address </h2>");
+//   } else {
+//     $('textarea').text("The message must be more than 40 characters:");
+//     // $("textarea").attr("placeholder", "The message must be more than 40 characters:");
+// }
+// });
