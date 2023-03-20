341a2b947fc5d59a04ae9fb443179e86e3b26007
ChVikkk <114808096+turelyk-arsen@users.noreply.github.com>
2023-03-20 15:23:16 +0100

    20/03/2023 electron

---
+++ Frontend/JAVASCRIPT/17-Software/2-Electron/course.md
@@ -0,0 +1,9 @@
+in folder 
+1. npm init
+2. npm install electron
+3. change package.json 
+ "main": "main.js",
+  and 
+"scripts": {
+    "start": "electron ."
+    
---
+++ Frontend/JAVASCRIPT/17-Software/2-Electron/main.js
@@ -0,0 +1,9 @@
+const {app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+     const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })   
+    win.loadFile('index.html')
+})
